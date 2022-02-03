12f649a114ecba76d77a70592549850cd6dd3973
Neil Weightman <82580266+NeilWeightman@users.noreply.github.com>
2022-02-03 17:33:27 +0000

    Update MySorter.java

--- NestedClasses/src/main/java/com/sparta/nested/MySorter.java
+++ NestedClasses/src/main/java/com/sparta/nested/MySorter.java
@@ -5,17 +5,29 @@ import java.util.Comparator;
 import java.util.List;
 
 public class MySorter<Zed> {
-    private class CustomerComparator implements Comparator<Customer> {
-        @Override
-        public int compare(Customer o1, Customer o2) {
-            return o1.getLastName().compareTo(o2.getLastName());
-        }
-    }
+//    private class CustomerComparator implements Comparator<Customer> {
+//        @Override
+//        public int compare(Customer o1, Customer o2) {
+//            return o1.getLastName().compareTo(o2.getLastName());
+//        }
+//    }
     public <Zed extends Comparable<Zed>> void sort(List<Zed> theList){
         Collections.sort(theList);
     }
     public void sortCustomers(List<Customer> theList){
-        CustomerComparator c = new CustomerComparator();
-        Collections.sort(theList, c);
+        //CustomerComparator c = new CustomerComparator();
+        // using anonymous inner class
+//        Collections.sort(theList, new Comparator<Customer>() {
+//            @Override
+//            public int compare(Customer o1, Customer o2) {
+//                return o1.getLastName().compareTo(o2.getLastName());
+//            }
+//        });
+
+        // using lambda expression
+//        Collections.sort(theList, (Customer o1, Customer o2) -> o1.getLastName().compareTo(o2.getLastName()));
+
+        // using method reference
+        Collections.sort(theList, Comparator.comparing(Customer::getLastName));
     }
 }
